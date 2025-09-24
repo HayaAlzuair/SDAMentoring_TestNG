4e110848f193dbea84d2147edef4bea7a6b06d7e
haya <h2000hhh@gmail.com>
2025-09-24 15:50:00 +0300

    1

---
+++ src/test/java/tests/C08_SwagLabsParameters.java
@@ -0,0 +1,39 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import utillites.TestBase;
+
+public class C08_SwagLabsParameters extends TestBase {
+
+
+       /*
+    Go to https://www.saucedemo.com/v1/index.html
+    Enter username by taking it as parameter
+    Enter password by taking it as parameter
+    Assert login
+     */
+
+    @Test
+    @Parameters({"username","password"})
+    void swagLabsParametersTest(@Optional("performance_glitch_user")String username,
+                                @Optional("secret_sauce")String password) {
+//        Go to https://www.saucedemo.com/v1/index.html
+        driver.get("https://www.saucedemo.com/v1/index.html");
+
+//        Enter username by taking it as parameter
+        driver.findElement(By.id("user-name")).sendKeys("performance_glitch_user");
+        driver.findElement(By.id("password")).sendKeys(password, Keys.ENTER);
+
+//        Enter password by taking it as parameter
+
+
+//        Assert login
+        assert driver.findElement(By.xpath("//*[.='Products']\n")).isDisplayed();
+
+    }
+
+}
